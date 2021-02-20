b29a6f5a4211e12bbfce5a0b73883046dac98b8a
Advait123-ctrl <79075926+Advait123-ctrl@users.noreply.github.com>
2021-02-19 21:55:53 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+inputs = []
+function get(){
+for(var i = 1 ; i <=6 ; i++){
+var t1 = t1.value
+inputs.push(t1)
+console.log(real)
+document.getElementById("real").innerHTML = inputs.join(". ")
+}
+}
