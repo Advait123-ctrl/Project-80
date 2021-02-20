2db6a3e8e0b0d929b578264bf2979aa396af5e0c
Advait123-ctrl <79075926+Advait123-ctrl@users.noreply.github.com>
2021-02-19 22:41:59 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 inputs = []
-function get(){
+inputs2 = []
+function getparagraph(){
 for(var i = 1 ; i <=6 ; i++){
-var t1 = t1.value
-inputs.push(t1)
-console.log(real)
-document.getElementById("real").innerHTML = inputs.join(". ")
+inputs.push(document.getElementById("t" + i).value);
+inputs.join(". ")
+document.getElementById("show").innerHTML = inputs.join(". ")
 }
 }
+function getparagraph2(){
+for(var i = 1 ; i <=6 ; i++){
+    inputs2.push(document.getElementById("e" + i).value);
+    inputs2.join(". ")
+    document.getElementById("show2").innerHTML = inputs2.join(". ")
+    }
+}
+
